564f26d1cf604997330ecfe3ddb93fe19b7a50b6
odan <odan3240@gmail.com>
2014-12-24 15:21:19 +0900

    Config.hppを追加

---
+++ PokeRNG/Config.hpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014 odan
+ * License: MIT License
+ */
+
+#ifndef POKERNG_CONFIG_HPP
+#define POKERNG_CONFIG_HPP
+namespace PokeRNG {
+    typedef uint64_t u64;
+    typedef uint32_t u32;
+} // end PokeRNG
+
+#endif
