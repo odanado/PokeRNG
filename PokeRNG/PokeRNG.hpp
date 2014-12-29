c7ee4d44a7310389c8dbcf897a6c5ad107798909
odan <odan3240@gmail.com>
2014-12-29 20:35:16 +0900

    PokeRNG.hppを追加

---
+++ PokeRNG/PokeRNG.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014 odan
+ * License: MIT License
+ */
+
+#ifndef POKERNG_POKERNG_HPP
+#define POKERNG_POKERNG_HPP
+
+#include "Calc5GenSeed.hpp"
+#include "CalcOffset.hpp"
+#include "Config.hpp"
+#include "ConstantParameters5Gen.hpp"
+#include "DateTime.hpp"
+#include "DateTimeIterator.hpp"
+#include "DateTimeRange.hpp"
+#include "LCG.hpp"
+#include "MT.hpp"
+#include "Parameters5Gen.hpp"
+#include "PokeRNG.hpp"
+#include "ROMType.hpp"
+
+#endif
+
