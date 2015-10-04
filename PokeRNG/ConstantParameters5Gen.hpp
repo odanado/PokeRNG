f8ca0c0d9710cacff3de311348d884a6277ecb57
odan <odan3240@gmail.com>
2015-10-05 08:43:03 +0900

    ConstantParameter5Genの実体を追加

--- PokeRNG/ConstantParameters5Gen.hpp
+++ PokeRNG/ConstantParameters5Gen.hpp
@@ -13,18 +13,42 @@ namespace PokeRNG {
 template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
 class ConstantParameters5Gen {
 public:
-    static const u32 nazo1 = nazo1_;
-    static const u32 nazo2 = nazo2_;
-    static const u32 nazo3 = nazo3_;
-    static const u32 nazo4 = nazo4_;
-    static const u32 nazo5 = nazo5_;
+    static const u32 nazo1;
+    static const u32 nazo2;
+    static const u32 nazo3;
+    static const u32 nazo4;
+    static const u32 nazo5;
 
-    static const u32 vcount = vcount_;
-    static const u32 gxstat = gxstat_;
-    static const u32 frame = frame_;
-    static const u32 timer0_min = timer0_min_;
-    static const u32 timer0_max = timer0_max_;
+    static const u32 vcount;
+    static const u32 gxstat;
+    static const u32 frame;
+    static const u32 timer0_min;
+    static const u32 timer0_max;
 };
+
+
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::nazo1 = nazo1_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::nazo2 = nazo2_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::nazo3 = nazo3_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::nazo4 = nazo4_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::nazo5 = nazo5_;
+
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::vcount = vcount_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::gxstat = gxstat_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::frame = frame_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::timer0_min = timer0_min_;
+template<u32 nazo1_, u32 nazo2_, u32 nazo3_, u32 nazo4_, u32 nazo5_, u32 vcount_, u32 gxstat_, u32 frame_, u32 timer0_min_, u32 timer0_max_>
+const u32 ConstantParameters5Gen<nazo1_, nazo2_, nazo3_, nazo4_, nazo5_, vcount_, gxstat_, frame_, timer0_min_, timer0_max_>::timer0_max = timer0_max_;
+
 
 } // end PokeRNG
 
